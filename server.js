e7e8c32571f0628ef5efac8ee977d040004ed623
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-20 18:58:23 -0500

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const SocketIO = require("socket.io");
 const http = require("http");
+const cors = require("cors")
+
+
 const port = 8080;
 const app = express();
+
+app.use(cors())
+ 
 const server = http.createServer(app);
 const io = SocketIO(server, {
   cors: {
