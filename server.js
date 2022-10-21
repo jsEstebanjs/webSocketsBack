09dccd3346c6ed316abefc2944980f7ebc99fd38
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-21 08:27:20 -0500

    add OPTIONS

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = SocketIO(server, {
     handlePreflightRequest: (req, res) => {
       res.writeHead(200, {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
         "Access-Control-Allow-Headers": "my-custom-header",
         "Access-Control-Allow-Credentials": true,
       });
