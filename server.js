6d0d2183938b11d776d6dae258190bbda76b5384
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-10-20 20:43:05 -0500

    nuevamente

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const SocketIO = require("socket.io");
 const http = require("http");
-const cors = require("cors")
-
+const cors = require("cors");
 
 const port = 8080;
 const app = express();
 
 app.use(cors())
-
 const server = http.createServer(app);
-const io = SocketIO(server);
+const io = SocketIO(server ,{
+  cors: {
+    origin: "https://websocketsfront.vercel.app/",
+    method: ["GET", "POST"]
+  }
+});
 
 io.on("connection", (socket) => {
-
   socket.on("message", (data) => {
     io.emit("broadcast", data);
   });
-
-  
-
-
 });
 
 server.listen(port, () => {
